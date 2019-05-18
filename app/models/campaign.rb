6e26aeff6f80af0062bf5ecbe7d6d58fe8b0bf0d
jeremira <jmiraille@gmail.com>
2019-05-18 10:07:24 +0200

    scope for network name

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,28 @@
 class Campaign < ApplicationRecord
   validates :network_criteria, presence: true
+
+  #
+  # Return Campaigns matching a peculiar network
+  #
+  scope :matching, ->(network) do
+    network_name(network.name).views_less_than(network.views_count)
+  end
+
+  scope :network_name,  ->(name) { where(network_criteria: name.to_s) }
+  scope :views_less_than, ->(count) { where(Campaign.arel_table[:views_criteria].lteq(count.to_i))}
+
+  # Campaign.per_network(network)
+  #
+  #User.arel_table[:id].gt(200)
+  # return Campaign qui match User.networks
+  #
+  # Campaign.where(network_criteria: network.name)
+  #         .where(views_criteria: > network.views_count)
+  #
+  # query = Campaign.all
+  #
+  # User.networks.each do |network|
+  #   Campaign.per_network(network)
+  # end
+
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -11,4 +11,123 @@ RSpec.describe Campaign, type: :model do
       expect(build :campaign, network_criteria: nil).not_to be_valid
     end
   end
+
+  describe ".matching" do
+    let(:network) {create :network, name: "youtube", views_count: 500}
+    let(:match_name_campaign) {create :campaign, network_criteria: "youtube"}
+    let(:match_count_campaign) {create :campaign, views_criteria: 500}
+    let(:matching_campaign) {create :campaign, network_criteria: "youtube", views_criteria: 500}
+    let(:campaign) {create :campaign}
+    let(:on_test) {Campaign.matching(network)}
+
+    before do
+      network ; match_name_campaign ; match_count_campaign ; matching_campaign ; campaign
+    end
+
+    it "return a relation" do
+      expect(on_test).to be_an ActiveRecord::Relation
+    end
+    it "return correct size relation" do
+      expect(on_test.size).to eq 1
+    end
+    it "return the matching campaign" do
+      expect(on_test).to include matching_campaign
+    end
+  end
+
+  describe ".matching_network" do
+    let(:on_test) {Campaign.network_name("youtube")}
+    let(:matching_campaign) {create :campaign, network_criteria: "youtube"}
+    let(:campaign) {create :campaign}
+
+    context "with no campaign" do
+      it "return an ActiveRecord relation" do
+        expect(on_test).to be_an ActiveRecord::Relation
+      end
+      it "return an empty relation" do
+        expect(on_test.size).to eq 0
+      end
+    end
+    context "with a non-matching campaign" do
+      before { campaign }
+      it "return an ActiveRecord relation" do
+        expect(on_test).to be_an ActiveRecord::Relation
+      end
+      it "return an empty relation" do
+        expect(on_test.size).to eq 0
+      end
+    end
+    context "with a matching campaign" do
+      before { matching_campaign }
+      it "return an ActiveRecord relation" do
+        expect(on_test).to be_an ActiveRecord::Relation
+      end
+      it "return a size 1 relation" do
+        expect(on_test.size).to eq 1
+      end
+      it "include correct campaign" do
+        expect(on_test).to include matching_campaign
+      end
+    end
+    context "with matching and non-matching campaign" do
+      before { matching_campaign ; campaign}
+      it "return an ActiveRecord relation" do
+        expect(on_test).to be_an ActiveRecord::Relation
+      end
+      it "return a size 1 relation" do
+        expect(on_test.size).to eq 1
+      end
+      it "include correct campaign" do
+        expect(on_test).to include matching_campaign
+      end
+    end
+  end
+
+  describe ".matching_views" do
+    let(:on_test) {Campaign.views_less_than(500)}
+    let(:matching_campaign) {create :campaign, views_criteria: 500}
+    let(:campaign) {create :campaign, views_criteria: 501}
+
+    context "with no campaign" do
+      it "return an ActiveRecord relation" do
+        expect(on_test).to be_an ActiveRecord::Relation
+      end
+      it "return an empty relation" do
+        expect(on_test.size).to eq 0
+      end
+    end
+    context "with a non-matching campaign" do
+      before { campaign }
+      it "return an ActiveRecord relation" do
+        expect(on_test).to be_an ActiveRecord::Relation
+      end
+      it "return an empty relation" do
+        expect(on_test.size).to eq 0
+      end
+    end
+    context "with a matching campaign" do
+      before { matching_campaign }
+      it "return an ActiveRecord relation" do
+        expect(on_test).to be_an ActiveRecord::Relation
+      end
+      it "return a size 1 relation" do
+        expect(on_test.size).to eq 1
+      end
+      it "include correct campaign" do
+        expect(on_test).to include matching_campaign
+      end
+    end
+    context "with matching and non-matching campaign" do
+      before { matching_campaign ; campaign}
+      it "return an ActiveRecord relation" do
+        expect(on_test).to be_an ActiveRecord::Relation
+      end
+      it "return a size 1 relation" do
+        expect(on_test.size).to eq 1
+      end
+      it "include correct campaign" do
+        expect(on_test).to include matching_campaign
+      end
+    end
+  end
 end
